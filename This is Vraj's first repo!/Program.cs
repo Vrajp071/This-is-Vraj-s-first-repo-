b002c823c670b19de524cf2da39492d81f241fef
bl4ck0uc4st <34685771+bl4ck0uc4st@users.noreply.github.com>
2024-03-21 12:27:35 -0600

    Add project files.

---
+++ This is Vraj's first repo!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Vraj's first repo!");
+        }
+    }
+}
